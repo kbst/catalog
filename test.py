63e8f09d460c4608432128578f0dd0d751643d27
Philipp Strube <pst@kubestack.com>
2019-04-12 14:04:18 +0200

    Create one archive per entry instead of one per variant

--- test.py
+++ test.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
 
 from os import listdir
-from os.path import isdir
+from os.path import isdir, join
 from subprocess import CalledProcessError, run
 
 DISTDIR = '_dist'
 
 failed_builds = []
 
-for dirname in listdir(f'{DISTDIR}'):
-    build_path = f'{DISTDIR}/{dirname}'
-    if not isdir(build_path):
+for entry_dir in listdir(DISTDIR):
+    entry_path = join(DISTDIR, entry_dir)
+    if not isdir(entry_path):
         continue
 
-    try:
-        build = run(
-            ['kustomize', 'build', build_path],
-            check=True,
-            capture_output=True,
-            text=True)
-    except CalledProcessError as failed_build:
-        print(f"\n\n[ERROR] `{dirname}` build failed\n",
-              f"  {failed_build}\n",
-              f"  {failed_build.stderr}")
-        failed_builds.append(dirname)
-    else:
-        print(f"[INFO] `{dirname}` build succesful")
+    for build_dir in listdir(entry_path):
+        build_path = join(DISTDIR, build_dir)
+        if not isdir(build_path):
+            continue
+
+        try:
+            build = run(
+                ['kustomize', 'build', build_path],
+                check=True,
+                capture_output=True,
+                text=True)
+        except CalledProcessError as failed_build:
+            print(f"\n\n[ERROR] `{entry_dir}` `{build_dir}` build failed\n",
+                  f"  {failed_build}\n",
+                  f"  {failed_build.stderr}")
+            failed_builds.append(f"{entry_dir} {build_dir}")
+        else:
+            print(f"[INFO] `{entry_dir}` `{build_dir}` build succesful")
 
 if len(failed_builds) != 0:
     number = len(failed_builds)
