4736146acc86308126fcdd9dc281e4efe3c85232
Philipp Strube <pst@kubestack.com>
2019-11-04 21:55:08 +0100

    Release tektoncd-v0.8.0-kbst.0

--- src/tektoncd/_updater/run.sh
+++ src/tektoncd/_updater/run.sh
@@ -7,4 +7,4 @@ source_path=$1
 target_path=$2
 version=$3
 
-curl -Lo $target_path/base/release.yaml https://storage.googleapis.com/tekton-releases/previous/$version/release.yaml
+curl -Lo $target_path/base/release.yaml https://github.com/tektoncd/pipeline/releases/download/$version/release.yaml
