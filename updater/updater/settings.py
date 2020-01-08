f5476b6b87d32511846cdba2b2212769fdb8e68d
Philipp Strube <pst@kubestack.com>
2020-01-08 21:32:10 +0100

    Switch from git to https for repo urls to use preconfigured auth

--- updater/updater/settings.py
+++ updater/updater/settings.py
@@ -1,5 +1,5 @@
 from os import getenv
 
 CATALOG_NAME = getenv('CATALOG_NAME', 'catalog')
-CATALOG_URL = getenv('CATALOG_URL', 'git@github.com:kbst/catalog.git')
+CATALOG_URL = getenv('CATALOG_URL', 'https://github.com/kbst/catalog.git')
 CATALOG_REF = getenv('CATALOG_REF', 'master')
