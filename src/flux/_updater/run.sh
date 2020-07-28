498ecfada2a9eb8719fc52fc3bdea3145764acb8
Philipp Strube <pst@kubestack.com>
2020-07-28 11:10:09 +0200

    Add Flux to catalog

---
+++ src/flux/_updater/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+source_path=$1
+target_path=$2
+version=$3
+
+cp $source_path/deploy/* $target_path/base/
+
+cd $target_path/base/
+
+# set commonAnnotations
+kustomize edit add annotation catalog.kubestack.com/heritage:kubestack.com/catalog/flux,catalog.kubestack.com/variant:base:app.kubernetes.io/version:$3
+
+# set commonLabels
+kustomize edit add label app.kubernetes.io/component:controller,app.kubernetes.io/managed-by:kubestack,app.kubernetes.io/name:flux
