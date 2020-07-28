b2f33463c65beaf4677d28915e59155ce7e1c358
Philipp Strube <pst@kubestack.com>
2020-07-28 13:45:34 +0200

    Fix path to upstream release artifact and changed image registry

--- src/nginx/_updater/run.sh
+++ src/nginx/_updater/run.sh
@@ -3,12 +3,5 @@
 set -e
 source_path=$1
 target_path=$2
-tag=$3
 
-cp $source_path/deploy/static/mandatory.yaml $target_path/base/mandatory.yaml
-
-# fix upstream manifests using master as image tag
-cd $target_path/base
-# upstream uses nginx- prefixed git tags, but no prefix for image tags
-image_tag=$(echo "$tag" | sed -e "s/^nginx-//")
-kustomize edit set image quay.io/kubernetes-ingress-controller/nginx-ingress-controller:$image_tag
+cp $source_path/deploy/static/provider/cloud/deploy.yaml $target_path/base/mandatory.yaml
