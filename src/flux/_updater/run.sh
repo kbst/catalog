e6d45f73de593b78e70038e93ccd1f6747c76736
Philipp Strube <pst@kubestack.com>
2020-08-13 11:00:59 +0200

    Fix concatenated variant and version annotations

--- src/flux/_updater/run.sh
+++ src/flux/_updater/run.sh
@@ -10,7 +10,7 @@ cp $source_path/deploy/* $target_path/base/
 cd $target_path/base/
 
 # set commonAnnotations
-kustomize edit add annotation catalog.kubestack.com/heritage:kubestack.com/catalog/flux,catalog.kubestack.com/variant:base:app.kubernetes.io/version:$3
+kustomize edit add annotation catalog.kubestack.com/heritage:kubestack.com/catalog/flux,catalog.kubestack.com/variant:base,app.kubernetes.io/version:$version
 
 # set commonLabels
 kustomize edit add label app.kubernetes.io/component:controller,app.kubernetes.io/managed-by:kubestack,app.kubernetes.io/name:flux
