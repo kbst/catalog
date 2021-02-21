72367a510c0795fe90f3fc783976c94434407340
Philipp Strube <pst@kubestack.com>
2021-02-21 09:14:54 +0100

    Require 0.4.1 version to allow multiple modules

--- src/_terraform_module/versions.tf
+++ src/_terraform_module/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kustomization = {
       source  = "kbst/kustomization"
-      version = ">= 0.4.0"
+      version = ">= 0.4.1"
     }
   }
   required_version = ">= 0.13"
