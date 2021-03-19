bcdc64127b532f99f2ec76877279c8fb725c1fe5
Philipp Strube <pst@kubestack.com>
2021-03-19 09:54:05 +0100

    Bump min. required Terraform and provider versions
     * Terraform to get the fix for default object values
     * Kustomization provider to get the fix for the fatal map access

--- src/_terraform_module/versions.tf
+++ src/_terraform_module/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     kustomization = {
       source  = "kbst/kustomization"
-      version = ">= 0.4.1"
+      version = ">= 0.4.3"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 0.15"
   experiments      = [module_variable_optional_attrs]
 }
