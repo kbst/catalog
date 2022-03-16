142ae068b3bb5e6e15410c26e2d310a16b5ca714
Philipp Strube <pst@kubestack.com>
2022-03-16 22:15:19 +0100

    Remove namespaced ArgoCD variants, due to missing CRDs
    Upstream removed the CRDs from the namespaced manifests, which
    makes sense. But for Kubestack the focus is on cluster-wide
    ArgoCD.
    
    Users that prefer to install the namespaced variants,
    can do so using the custom-manifests module. Use the module
    to install the CRDs and then again once per namespace you
    want a dedicated ArgoCD in.

--- src/argo-cd/_updater/run.sh
+++ src/argo-cd/_updater/run.sh
@@ -8,11 +8,5 @@ version=$3
 # copy install.yaml
 cp $source_path/manifests/install.yaml $target_path/normal/install.yaml
 
-# copy namespace-install.yaml
-cp $source_path/manifests/namespace-install.yaml $target_path/normal-namespaced/namespace-install.yaml
-
 # copy ha/install.yaml
 cp $source_path/manifests/ha/install.yaml $target_path/ha/install.yaml
-
-# copy ha/namespace-install.yaml
-cp $source_path/manifests/ha/namespace-install.yaml $target_path/ha-namespaced/namespace-install.yaml
